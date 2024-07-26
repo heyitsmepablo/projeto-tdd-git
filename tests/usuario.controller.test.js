79173ad3c8b5e9aed11c918565eb8490b0c3a5b3
heyitsmepablo <pabloeduardossantos@gmail.com>
2024-07-26 18:48:53 -0300

    test:  testes unitarios usuario/novaConta

--- tests/usuario.controller.test.js
+++ tests/usuario.controller.test.js
@@ -4,7 +4,7 @@ import app from "../src/index.js";
 describe('POST usuario/novaConta : METHOD create', ()=>{
 
     it('Pass 200: O usuario deve ser criado',async ()=>{
-        const body = {
+        const bodyRequest = {
             nome: 'Pablo',
             email: 'p@p.com',
             senha: '123',
@@ -13,15 +13,53 @@ describe('POST usuario/novaConta : METHOD create', ()=>{
 
         const res = await supertest(app)
         .post('/usuario/novaConta')
-        .send(body)
-        .expect((res)=>{
-            const {status,body} = res
+        .send(bodyRequest)
+
+        const {status} = res
+        const bodyResponse = res.body
+
+        try {
+                
+            expect(status).toBe(200)
+            expect(bodyResponse).toHaveProperty('message')
+            expect(bodyResponse.message).toBe("sucesso")
             
-            if (status != 200){
-                throw new Error(`Esperado 200, Recebido ${status} ( Body: ${JSON.stringify(body)} )`)
+            
+            } catch (/** @type {any} */ error) {
+                const details = {details: bodyResponse}
+                throw new Error(`${error.message} \n  ${JSON.stringify(details,null,2)}`)
             }
-        })
+    })
+
+    it('Error 400: Todos os campos devem ser obrigatorios', async ()=>{
+        
+        const bodyRequest = 
+            {
+                nome:'' ,
+                email:'' ,
+                senha:'' ,
+                dataNascimento:'' 
+            }
+            
+        const res = await supertest(app)
+        .post('/usuario/novaConta')
+        .send(bodyRequest)
+        
+        const {body,status} = res
+        
+        try {
+                
+        expect(status).toBe(400)
+        expect(body).toHaveProperty('message')
+        expect(body.message).toBe('Os campos são obrigatorios')
+            
+        } catch (/** @type {any} */ error) {
+            const details = {details: body}
+            throw new Error(`${error.message} \n  ${JSON.stringify(details,null,2)}`)
+        }
+
 
     })
 
+   
 })
